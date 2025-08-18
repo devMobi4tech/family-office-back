33f1b7a84af162ff8c0a58bce60b575cb3b867c2
João Victor <joao.dipaay@gmail.com>
2025-08-18 20:11:49 -0300

    chore: conectando mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'family_office',
+      entities: [],
+      synchronize: true, // TODO: Setar como false em produção
+    }),
+  ],
 })
 export class AppModule {}
